586be9d448156933cf5d85cc2916728c029ac003
Koshou <76564170+koshou1010@users.noreply.github.com>
2022-05-25 15:43:28 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import re
 import os
-import datetime
+
 
 class TextTransformerType1:
     #2019/5/8 上午 11:30:34	0.454244	1.338773	0.599610	0.651245	0.944937	0.519692	0.533834	3.466636	3.102729	3.667746	2.939275	0.249186	1.352257	1.785888	1.388902	57.900000
@@ -23,7 +23,12 @@ class TextTransformerType1:
         self.make_fake_preset_meta()
         self.open_file()
     
+    
     def open_file(self):
+        '''
+        use for open file and read eachline
+        '''
+        
         with open(self.input_file, 'r') as f:
             content = f.readlines()
             for index, each_line in enumerate(content):
@@ -66,6 +71,10 @@ class TextTransformerType1:
             f.write('","status_flag":"on_first_preset_voltage","subject_flag":"auto","valve":0,"pump":1,"step_flag":null}\n')
     
     def save_data(self,each_line):
+        '''
+        use for save eachline data to ndjson format
+        '''
+        
         #{"name":"data","meta_index":-1,"l_date":"2022-04-01T11:24:37.736+08:00","channel":[1.58025,1.628,1.525,1.5405,1.55625,1.53975,1.575,3.238,3.46275,3.3645,3.418,1.90725,1.5195,1.976,0.520325,40.676193,0.33]}
         data_list = [f"{i}" for i in each_line.replace('\n', '').split('\t')[1:]]
         date = each_line.split('\t')[0].replace('/', '-').split(' ')[0]+"T"+each_line.split('\t')[0].replace('/', '-').split(' ')[-1]+".000+08:00"
@@ -84,6 +93,10 @@ class TextTransformerType1:
             
             
     def find_first_time(self):
+        '''
+        use for find first time to file meta use
+        '''
+        
         with open(self.input_file, 'r') as f:
             content = f.readlines()
             for index, each_line in enumerate(content):
@@ -92,6 +105,10 @@ class TextTransformerType1:
                 break
             
     def make_fake_preset_meta(self):
+        '''
+        use for make preset meta, if not preset meta, ESRTSD can't transfer voltage to resistance
+        '''
+        
         #{"name":"preset_meta","meta_index":-1,"l_date":"2022-03-21T11:52:53.436+08:00","len_channel":17,"device_cfg":{"supply_voltage":5.0,"channel_biasing_resistance":[12469.0,10906.0,31219.0,26141.0,17547.0,23016.0,10125.0,3875.0,32000.0,8172.0,11688.0,2312.0,53875.0,2312.0,null,null,null],"channel_reference_c":[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],"channel_reference_voltage":[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],"preset_flow":[5.0],"channel_actual_gain":[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,null,null,null],"channel_preset_voltage":[1.5,1.5,1.5,1.5,1.5,1.5,1.5,3.5,3.5,3.5,3.5,1.5,1.5,1.5,null,null,null]},"critical":{"first_waiting_initial_stable":{"threshold_value":1.0,"threshold_rate":1.0,"timeout":1800.0,"during":1.0},"second_waiting_initial_stable":{"threshold_value":1.0,"threshold_rate":1.0,"timeout":10.0,"during":1.0},"round_waiting_initial_stable":{"threshold_value":1.0,"threshold_rate":1.0,"timeout":30.0,"during":1.0},"baseline":{"threshold_value":1.0,"threshold_rate":1.0,"timeout":60.0,"during":1.0},"stable":{"threshold_value":1.0,"threshold_rate":1.0,"timeout":120.0,"during":1.0},"reacting":{"threshold_value":1.0,"threshold_rate":1.0,"timeout":30.0,"during":1.0},"recovery":{"threshold_value":1.0,"threshold_rate":1.0,"timeout":600.0,"during":1.0},"auto_stop_valve":{"threshold_value":1.0,"threshold_rate":1.0,"timeout":4.0,"during":1.0},"slope_window_size":10,"threshold_ref_channel_indexes":[0,1]}}
         with open(self.output_file, "a") as f:
             f.write('{"name":"preset_meta","meta_index":-1,"l_date":"')
